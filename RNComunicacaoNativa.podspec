e3992d1280033561d4adaf8483a132640cf50893
m3n0r <wesleysd1989@gmail.com>
2019-11-09 23:19:49 -0300

    update

--- RNComunicacaoNativa.podspec
+++ RNComunicacaoNativa.podspec
@@ -6,13 +6,13 @@ Pod::Spec.new do |s|
   s.description  = <<-DESC
                   RNComunicacaoNativa
                    DESC
-  s.homepage     = ""
+  s.homepage     = "https://github.com/wesleysd1989/comunicacao-nativa"
   s.license      = "MIT"
   # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
   s.author             = { "author" => "author@domain.cn" }
   s.platform     = :ios, "7.0"
-  s.source       = { :git => "https://github.com/author/RNComunicacaoNativa.git", :tag => "master" }
-  s.source_files  = "RNComunicacaoNativa/**/*.{h,m}"
+  s.source       = { :git => "https://github.com/wesleysd1989/comunicacao-nativa", :tag => "master" }
+  s.source_files = "ios/*.{h,m}"
   s.requires_arc = true
 
 
--- ios/RNComunicacaoNativa.m
+++ ios/RNComunicacaoNativa.m
@@ -1,7 +1,5 @@
 
 #import "RNComunicacaoNativa.h"
-#import <React/RTCPromiseResolveBlock.h>
-#import <React/RTCPromiseRejectBlock.h>
 
 @implementation RNComunicacaoNativa
 
@@ -11,10 +9,16 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(checkBridge:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(checkBridge)
 {
-  resolve(@"modulo nativo");
+  [self emitMessageToRN:@"EXAMPLE_EVENT" :nil];
+}
+
+// Implement methods that you want to export to the native module
+- (void) emitMessageToRN: (NSString *)eventName :(NSDictionary *)params {
+  // The bridge eventDispatcher is used to send events from native to JS env
+  // No documentation yet on DeviceEventEmitter: https://github.com/facebook/react-native/issues/2819
+  [self sendEventWithName: eventName body: params];
 }
 
 @end
